65a64f2719b17604a4e481661565ed33d6c9ca09
loligoty@gmail.com <loligoty@gmail.com>
2025-08-28 08:45:53 +0300

    Add project files.

---
+++ Laba22OOp/Program.cs
@@ -0,0 +1,34 @@
+﻿public class StringWorker
+{
+    public void ProcessCapitalLetters(string originalString)
+    {
+        Console.WriteLine("Original string: " + originalString);
+        Console.WriteLine("Length: " + originalString.Length);
+
+        string processedString = new string(originalString.ToUpper().OrderBy(c => c).ToArray());
+        Console.WriteLine("Sorted and capitalized: " + processedString);
+        Console.WriteLine();
+    }
+
+    public void ProcessSmallLetters(string originalString)
+    {
+        Console.WriteLine("Original string: " + originalString);
+        Console.WriteLine("Length: " + originalString.Length);
+
+        string processedString = new string(originalString.ToLower().OrderByDescending(c => c).ToArray());
+        Console.WriteLine("Sorted and lowercased: " + processedString);
+        Console.WriteLine();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        StringWorker worker = new StringWorker();
+
+        worker.ProcessCapitalLetters("Salam Popolam");
+
+        worker.ProcessSmallLetters("Popolam Salam");
+    }
+}
